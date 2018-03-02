c6d362d211ce860b88ea9a1637e25f2ddb4d19d9
Jamie Heller <jamie@driveclutch.com>
2018-03-02 10:56:41 -0500

    Actually capture exit code of liquibase command [#155595308]

--- update.sh
+++ update.sh
@@ -1,33 +1,39 @@
+#!/bin/sh
 # Run liquibase, posting result to slack webhook if $SLACK_WEBHOOK is set
 
-# Init fd 7 has duplicate of stdout
-exec 7>&1
+# Function to actually run the liquibase command
+run_liquibase() { 
+  /app/liquibase/liquibase \
+	  --changeLogFile=changelog.xml \
+	  --driver=org.postgresql.Driver \
+	  --classpath=/app/jdbc_drivers/postgresql-42.1.4.jar \
+	  --url=jdbc:postgresql://${PGS_HOST}:${PGS_PORT}/${PGS_DB} \
+	  --defaultSchemaName=${SCHEMA} \
+	  --username=${PGS_USERNAME} \
+	  --password=${PGS_PASSWORD} \
+	  update 2>&1
+}
 
-# Run the liquibase command, teed to stdout and captured in $MY_OUTPUT via fd 7
-MY_OUTPUT=$(/app/liquibase/liquibase \
-  --changeLogFile=changelog.xml \
-  --driver=org.postgresql.Driver \
-  --classpath=/app/jdbc_drivers/postgresql-42.1.4.jar \
-  --url=jdbc:postgresql://${PGS_HOST}:${PGS_PORT}/${PGS_DB} \
-  --defaultSchemaName=${SCHEMA} \
-  --username=${PGS_USERNAME} \
-  --password=${PGS_PASSWORD} \
-  update 2>&1 | tee /dev/fd/7
-)
+# Initialize our pipe for redirection
+mkfifo my_pipe
+# Tee to stdout and capture in log file
+tee liquibase.log < my_pipe &
 
-# Capture exit code of liquibase command (although liquibase itself always returns 0 :( )
+# Run the liquibase command through the pipe
+run_liquibase > my_pipe
+# Capture exit code of liquibase command
 MY_EXIT_CODE=$?
 
 if [[ ! -z "$SLACK_WEBHOOK" ]]; then
+    # Get ahold of log for slack message
+    MY_OUTPUT=$(cat liquibase.log)
+
     ESCAPED_MSG=$(echo "$MY_OUTPUT" | sed 's/"/\"/g' | sed "s/'/\'/g" )
 
     JSON="{\"channel\": \"$CHANNEL\", \"username\": \"deployinator\", \"icon_emoji\": \":rocket:\", \"attachments\": [{\"color\": \"danger\", \"text\": \"<!here> ${HOSTNAME}\n\`\`\`${ESCAPED_MSG}\`\`\`\"}]}"
 
     curl -s -d "payload=${JSON}" "$SLACK_WEBHOOK"
 fi
-
-# close fd 7
-exec 7>&-
 
 # Exit with the liquibase exit code
 exit $MY_EXIT_CODE
