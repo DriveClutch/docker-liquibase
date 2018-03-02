a8abbb0c9aef6a9babecbf59c989e524e79a55a8
Jamie Heller <jamie@driveclutch.com>
2018-03-02 11:23:21 -0500

    Capture changeset for slack message, write update sql to logs
    [#155595308]

--- update.sh
+++ update.sh
@@ -1,32 +1,83 @@
 #!/bin/sh
 # Run liquibase, posting result to slack webhook if $SLACK_WEBHOOK is set
 
-# Function to actually run the liquibase command
-run_liquibase() { 
-  /app/liquibase/liquibase \
-	  --changeLogFile=changelog.xml \
-	  --driver=org.postgresql.Driver \
-	  --classpath=/app/jdbc_drivers/postgresql-42.1.4.jar \
-	  --url=jdbc:postgresql://${PGS_HOST}:${PGS_PORT}/${PGS_DB} \
-	  --defaultSchemaName=${SCHEMA} \
-	  --username=${PGS_USERNAME} \
-	  --password=${PGS_PASSWORD} \
-	  update 2>&1
+# Function to describe the changelogs that will be run
+diff_status() {
+	/app/liquibase/liquibase \
+		--changeLogFile=changelog.xml \
+		--driver=org.postgresql.Driver \
+		--classpath=/app/jdbc_drivers/postgresql-42.1.4.jar \
+		--url=jdbc:postgresql://${PGS_HOST}:${PGS_PORT}/${PGS_DB} \
+		--defaultSchemaName=${SCHEMA} \
+		--username=${PGS_USERNAME} \
+		--password=${PGS_PASSWORD} \
+		status --verbose 2>&1
 }
 
-# Initialize our pipe for redirection
-mkfifo my_pipe
-# Tee to stdout and capture in log file
-tee liquibase.log < my_pipe &
+# Function to output the SQL that will be run
+update_sql() {
+	/app/liquibase/liquibase \
+		--changeLogFile=changelog.xml \
+		--driver=org.postgresql.Driver \
+		--classpath=/app/jdbc_drivers/postgresql-42.1.4.jar \
+		--url=jdbc:postgresql://${PGS_HOST}:${PGS_PORT}/${PGS_DB} \
+		--defaultSchemaName=${SCHEMA} \
+		--username=${PGS_USERNAME} \
+		--password=${PGS_PASSWORD} \
+		updateSQL 2>&1
+}
 
+# Function to actually run the liquibase command
+run_liquibase() { 
+	/app/liquibase/liquibase \
+		--changeLogFile=changelog.xml \
+		--driver=org.postgresql.Driver \
+		--classpath=/app/jdbc_drivers/postgresql-42.1.4.jar \
+		--url=jdbc:postgresql://${PGS_HOST}:${PGS_PORT}/${PGS_DB} \
+		--defaultSchemaName=${SCHEMA} \
+		--username=${PGS_USERNAME} \
+		--password=${PGS_PASSWORD} \
+		update 2>&1
+}
+
+# Init the log file
+touch LOGFILE
+# Initialize our pipe for redirection
+mkfifo pipe0
+echo '
+**************************************************************
+Changeset Overview:
+**************************************************************' | tee -a LOGFILE
+# Tee to stdout and capture in log file
+tee -a LOGFILE < pipe0 &
+
+# Run diff status to see output of changelogs to run
+diff_status > pipe0
+
+# Just log the update sql, don't put in log for slack message
+echo '
+**************************************************************
+Liquibase update SQL
+**************************************************************'
+update_sql
+
+# Initialize our pipe for redirection
+mkfifo pipe1
+# Tee to stdout and capture in log file
+tee -a LOGFILE < pipe1 &
+# Delimiter in logfile
+echo '
+**************************************************************
+Liquibase run output:
+**************************************************************' | tee -a LOGFILE
 # Run the liquibase command through the pipe
-run_liquibase > my_pipe
+run_liquibase > pipe1
 # Capture exit code of liquibase command
 MY_EXIT_CODE=$?
 
 if [[ ! -z "$SLACK_WEBHOOK" ]]; then
     # Get ahold of log for slack message
-    MY_OUTPUT=$(cat liquibase.log)
+    MY_OUTPUT=$(cat LOGFILE)
 
     ESCAPED_MSG=$(echo "$MY_OUTPUT" | sed 's/"/\"/g' | sed "s/'/\'/g" )
 
@@ -37,3 +88,4 @@ fi
 
 # Exit with the liquibase exit code
 exit $MY_EXIT_CODE
+
