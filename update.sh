f445fde70e48af070bfa9114b1fa76e3193c3e91
Jamie Heller <jamie@driveclutch.com>
2018-03-08 18:30:41 -0500

    Add step to ensure schema exists before liquibase runs

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 # Run liquibase, posting result to slack webhook if $SLACK_WEBHOOK is set
 
 # Prefix stdout with 'SEV=WARN' to help delimit log messages
@@ -6,6 +6,18 @@ warnify() {
 	awk 'BEGIN {print "SEV=WARN"} {print $0}'
 }
 
+# Function to ensure the schema exists, in case this is the schema
+# initialization run, we need it to exist for liquibase to use it as
+# the default schema
+ensure_schema() {
+	PGPASSWORD="${PGS_PASSWORD}" psql \
+		--host="${PGS_HOST}" \
+		--port="${PGS_PORT}" \
+		--username="${PGS_USERNAME}" \
+		--command="CREATE SCHEMA IF NOT EXISTS ${SCHEMA};"
+}
+
+# Base liquibase helper
 liquibase() {
   /app/liquibase/liquibase \
 		--changeLogFile=changelog.xml \
@@ -19,7 +31,7 @@ liquibase() {
 		2>&1
 }
 
-# Function to describe the change sets that will be run
+# Function to describe the any unexpected change sets that will be run
 diff_unexpected() {
 	liquibase unexpectedChangeSets --verbose
 }
@@ -42,32 +54,39 @@ run_liquibase() {
 # Init the log file
 touch LOGFILE
 # Initialize our pipe for redirection
-if [[ -p "pipe0" ]]; then
-	rm pipe0
+if [[ -p "my_pipe" ]]; then
+	rm my_pipe
 fi
-mkfifo pipe0
+mkfifo my_pipe
+
+# Tee to stdout and capture in log file
+tee -a LOGFILE < my_pipe | warnify &
+
+# Ensure the schem exists
+echo '
+SEV=WARN
+**************************************************************
+Ensure schema with psql
+**************************************************************' | tee -a LOGFILE
+ensure_schema > my_pipe
 
 echo '
 SEV=WARN
 **************************************************************
 Unexpected Change Set Overview:
 **************************************************************' | tee -a LOGFILE
-# Tee to stdout and capture in log file
-tee -a LOGFILE < pipe0 | warnify &
 
 # Run diff status to see output of change sets to run
-diff_unexpected > pipe0
+tee -a LOGFILE < my_pipe | warnify & diff_unexpected > my_pipe
 
 echo '
 SEV=WARN
 **************************************************************
 Change Set Overview:
 **************************************************************' | tee -a LOGFILE
-# Tee to stdout and capture in log file
-tee -a LOGFILE < pipe0 | warnify &
 
 # Run diff status to see output of change sets to run
-diff_status > pipe0
+tee -a LOGFILE < my_pipe | warnify & diff_status > my_pipe
 
 # Just log the update sql, don't put in log for slack message
 echo '
@@ -77,22 +96,16 @@ Liquibase update SQL
 **************************************************************'
 update_sql | warnify
 
-# Initialize our pipe for redirection
-if [[ -p "pipe1" ]]; then
-	rm pipe1
-fi
-mkfifo pipe1
-# Tee to stdout and capture in log file
-tee -a LOGFILE < pipe1 | warnify &
-
 # Delimiter in logfile
 echo '
 SEV=WARN
 **************************************************************
 Liquibase run output:
 **************************************************************' | tee -a LOGFILE
+tee -a LOGFILE < my_pipe | warnify &
+
 # Run the liquibase command through the pipe
-run_liquibase > pipe1
+run_liquibase > my_pipe
 # Capture exit code of liquibase command
 MY_EXIT_CODE=$?
 
